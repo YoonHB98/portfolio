695fa3b8ec9cfba5f7b8cc8b4175acc659f279da
윤형빈 <wudqls98@gmail.com>
2022-03-03 22:36:28 +0900

    윤형빈 220303

---
+++ API/GameApp/main.cpp
@@ -0,0 +1,12 @@
+#include <Windows.h>
+
+#include <GameEngineBase/GameEngineWindow.h>
+
+int __stdcall WinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ char* lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    GameEngineWindow::GetInst().CreateGameWindow(hInstance);
+    GameEngineWindow::GetInst().ShowGameWindow();
+}
