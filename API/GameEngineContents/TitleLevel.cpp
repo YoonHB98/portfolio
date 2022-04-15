05d714602846ef43e024ddc7506ff7ce96870b67
윤형빈 <wudqls98@gmail.com>
2022-04-15 12:50:05 +0900

    타이틀

--- API/GameEngineContents/TitleLevel.cpp
+++ API/GameEngineContents/TitleLevel.cpp
@@ -4,6 +4,8 @@
 #include "TitleCharacter.h"
 #include <GameEngine/GameEngineImageManager.h>
 #include "Coin.h"
+#include "Point.h"
+#include "WorldCount.h"
 
 TitleLevel::TitleLevel()
 {
@@ -17,11 +19,18 @@ void TitleLevel::Loading()
 {
 
 
-
+	CreateActor<Coin>(1);
+	{
+		Point* Actor = CreateActor<Point>(1);
+	}
+	{
+		Point* Actor = CreateActor<Point>(1);
+		Actor->SetPosition(float4{562, 800});
+	}
 	CreateActor<TitleBackGround>(0);
 	CreateActor<TitleCharacter>(1);
 	CreateActor<Coin>(2);
-
+	WorldCount* UI = CreateActor<WorldCount>(2);
 }
 
 void TitleLevel::Update()
